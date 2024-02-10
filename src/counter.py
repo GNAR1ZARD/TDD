6a766653807ebd369b3134768172f27a95bf2cce
Gnarizard <toefoo@protonmail.com>
2024-02-09 16:12:53 -0800

    fixed linting errors

--- src/counter.py
+++ src/counter.py
@@ -1,29 +1,20 @@
-# counter.py
-
 from flask import Flask
-# we need to import the file that contains the status codes
 from src import status 
-
 
 app = Flask(__name__)
 
 COUNTERS = {}
 
-# We will use the app decorator and create a route called slash counters.
-# specify the variable in route <name>
-# let Flask know that the only methods that is allowed to called
-# on this function is "POST".
 @app.route('/counters/<name>', methods=['POST'])
 def create_counter(name):
     """Create a counter"""
     app.logger.info(f"Request to create counter: {name}")
     global COUNTERS
     if name in COUNTERS:
-        return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+        return {"Message": f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
 
-# Update counter:
 @app.route('/counters/<name>', methods=['PUT'])
 def update_counter(name):
     """Increment a counter"""
@@ -34,7 +25,6 @@ def update_counter(name):
     COUNTERS[name] += 1
     return {name: COUNTERS[name]}, status.HTTP_200_OK
 
-# Implement GET Method
 @app.route('/counters/<name>', methods=['GET'])
 def get_counter(name):
     """Get a counter's value"""
@@ -42,4 +32,3 @@ def get_counter(name):
     if name not in COUNTERS:
         return {"Message": "Counter does not exist"}, status.HTTP_404_NOT_FOUND
     return {name: COUNTERS[name]}, status.HTTP_200_OK
-
